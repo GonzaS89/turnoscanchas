e21522f634a805f802cfbcb4585dbf76430af5d8
GonzaS89 <gnzx89@gmail.com>
2025-05-30 22:54:39 -0300

    probando nueva interfaz

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,13 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cors());
 app.use(express.json())
 
